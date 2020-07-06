5ccecdfa7a693f8b23b5a5da51d98bc80e3e7037
TedisAgolli <tedisagolli@gmail.com>
2020-07-05 21:24:37 -0400

    Remove tooltip and change max link name length to 120, allowing it to wrap down

--- src/components/Bookmark.js
+++ src/components/Bookmark.js
@@ -2,44 +2,30 @@ import React from "react";
 import { Row, Button, Tooltip, OverlayTrigger } from "react-bootstrap";
 
 function Bookmark(props) {
-  const formatLink = (link) => {
-    if (link && link.length > 21) return link.substr(0, 21) + "...";
-    else return link;
+  const MAX_LINK_NAME_LENGTH = 120;
+  const formatLinkName = (linkName) => {
+    if (linkName && linkName.length > MAX_LINK_NAME_LENGTH)
+      return linkName.substr(0, MAX_LINK_NAME_LENGTH) + "...";
+    else return linkName;
   };
 
   const { linkHref, linkName } = props;
-  const otherLink = linkName.substr(0, 53) + "...";
-  const formattedLinkName = formatLink(linkName);
+  const formattedLinkName = formatLinkName(linkName);
   const deleteLink = () => props.deleteLink(linkHref);
 
-  const tooltip = <Tooltip>{otherLink}</Tooltip>;
   return (
     <Row className="mt-0">
       <table>
         <tr>
           <td>
-            <OverlayTrigger
-              id={linkHref}
-              placement="top"
-              overlay={tooltip}
-              popperConfig={{
-                modifiers: {
-                  preventOverflow: {
-                    boundariesElement: "viewport",
-                    enabled: false,
-                  },
-                },
-              }}
+            <Button
+              size="sm"
+              variant="outline-primary"
+              href={linkHref}
+              target="_blank"
             >
-              <Button
-                size="sm"
-                variant="outline-primary"
-                href={linkHref}
-                target="_blank"
-              >
-                {formattedLinkName}
-              </Button>
-            </OverlayTrigger>
+              {formattedLinkName}
+            </Button>
           </td>
           <td>
             <Button variant="danger" size="sm" onClick={deleteLink}>
