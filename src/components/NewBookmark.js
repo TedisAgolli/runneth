8e71186bc45b8780b87168896534a442a2534af3
TedisAgolli <tedisagolli@gmail.com>
2021-05-09 11:08:29 -0400

    Bump version, add empty state screenshot

--- src/components/NewBookmark.js
+++ src/components/NewBookmark.js
@@ -51,7 +51,7 @@ function NewBookmark(props) {
           value={linkName}
           onChange={handleLinkNameChange}
           class="mr-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-          placeholder="Link label"
+          placeholder="Bookmark Name"
         />
       </div>
       <button
